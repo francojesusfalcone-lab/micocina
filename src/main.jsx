c0753d766675970b6c82a99d7cab26433115251a
Franco Falcone <francojesusfalcone@gmail.com>
2026-03-18 18:55:56 -0300

    fix onboarding and settings

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Eliminar service workers viejos del PWA que causan pantalla blanca
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((sw) => sw.unregister())
+  })
+  window.caches?.keys().then((keys) => {
+    keys.forEach((key) => window.caches.delete(key))
+  })
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
