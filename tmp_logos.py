8adb9d52743c59e6d3be35aa278340ff467f9022
Franco Falcone <francojesusfalcone@gmail.com>
2026-03-19 00:08:53 -0300

    "cleanup"

--- tmp_logos.py
+++
@@ -1,6 +0,0 @@
-import shutil
-shutil.copy('/mnt/user-data/uploads/1773888362169_WhatsApp_Image_2026-03-18_at_10_58_46.jpeg',
-            r'C:\Users\franc\OneDrive\Escritorio\micocina\public\logo-icon.png')
-shutil.copy('/mnt/user-data/uploads/1773888362170_WhatsApp_Image_2026-03-18_at_10_58_45.jpeg',
-            r'C:\Users\franc\OneDrive\Escritorio\micocina\public\logo-full.png')
-print('ok')
